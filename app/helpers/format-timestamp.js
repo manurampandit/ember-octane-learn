2e0bc2b4a94fde4f50d700b33dc71323c9bc4e9e
Manu Pandit <mpandit@linkedin.com>
2019-10-09 11:30:16 +0530

    Step 4 basic helpers

---
+++ app/helpers/format-timestamp.js
@@ -0,0 +1,5 @@
+import { helper } from '@ember/component/helper';
+import { dateToString } from '../utils/date';
+export default helper(function formatTimestamp(params/*, hash*/) {
+  return dateToString(params[0]);
+});
---
+++ tests/integration/helpers/format-timestamp-test.js
@@ -0,0 +1,16 @@
+import { module, test } from 'qunit';
+import { setupRenderingTest } from 'ember-qunit';
+import { render } from '@ember/test-helpers';
+import hbs from 'htmlbars-inline-precompile';
+
+module('Integration | Helper | format-timestamp', function(hooks) {
+  setupRenderingTest(hooks);
+
+  // Replace this with your real tests.
+  test('it renders', async function(assert) {
+    this.set('inputValue', 'Oct 9 2019 10 AM');
+
+    await render(hbs`{{format-timestamp "Oct 9, 2019 10:00 AM"}}`);
+    assert.equal(this.element.textContent.trim(), 'Oct 9, 2019 10:00.00 AM');
+  });
+});
